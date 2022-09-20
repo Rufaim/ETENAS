55c69b314909a3b9b583c9398cc700c8c08db6c7
Rufaim <pavelrumyancew@yandex.ru>
2022-09-20 11:17:47 -0400

    refactoring main script

--- launch_prune_etenas.sh
+++ launch_prune_etenas.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-NUM_SAMPLES=${1:-250}
-SAVE_FOLDER=${2:-"./output/plotted_metrics"}
-BASE_DATAPATH=${3:-"./NAS_data"}
+SAVE_FOLDER=${1:-"./output/prune-etenas-nas-bench-201"}
+BASE_DATAPATH=${2:-"./NAS_data"}
 
 source ./venv/bin/activate
 
-#declare -a methods=(
+declare -a methods=(
 #    "mean,mean.json"
 #    "frobenius,frob.json"
 #    "condition,cond.json"
@@ -16,19 +15,30 @@ source ./venv/bin/activate
 #    "expected_relu_500,exp_500.json"
 #    "expected_relu_100,exp_100.json"
 #    "lga,lga.json"
-#    "frobenius+lga,frob_lga.json"
-#    "all,all.json"
-#)
-declare -a methods=(
-#  "mean_test,mean.json"
-  "eigenvalue_score_test,eig.json"
-)
 
+    "relu_distance+eigenvalue_score,dist_eig.json"
+    "expected_relu_100+relu_distance,exp_100_dist.json"
+    "expected_relu_100+eigenvalue_score,exp_100_eig.json"
+    "frob+relu_distance,frob_dist.json"
+    "frobenius+eigenvalue_score,frob_eig.json"
+    "frobenius+expected_relu_100,frob_exp_100.json"
+    "frobenius+expected_relu_100+relu_distance,frob_exp_100_dist.json"
+    "frobenius+lga,frob_lga.json"
+    "frobenius+mean,frob_mean.json"
+    "frobenius+mean+relu_distance,frob_mean_dist.json"
+    "frobenius+mean+expected_relu_100,frob_mean_exp_100.json"
+    "frobenius+mean+expected_relu_100+relu_distance,frob_mean_exp_100_dist.json"
+    "mean+relu_distance,mean_dist.json"
+    "mean+eigenvalue_score,mean_eig.json"
+    "mean+expected_relu_100,mean_exp_100.json"
+    "mean+expected_relu_100+relu_distance,mean_exp_100_dist.json"
+    "all,all.json"
+)
 
 
 for elem in ${methods[@]}
 do
-  IFS=",";read -a method <<< "$elem"
+  IFS=","; read -a method <<< "$elem"
   for dataset in "ImageNet16-120" "cifar10" "cifar100"; do
     if [ $dataset == "ImageNet16-120" ]; then
       data_path="ImageNet16"
@@ -41,11 +51,13 @@ do
       output_path="cifar10"
     fi
     for run in {1..10}; do
-      python3 ./prune_etenas.py --save_dir "./output/prune-etenas-nas-bench-201/${method[0]}/${output_path}" --max_nodes 4 \
-        --dataset ${dataset} --data_path "NAS_data/${data_path}" --search_space_name nas-bench-201 --super_type basic \
-        --arch_nas_dataset ./NAS_data/NAS-Bench-201-v1_0-e61699-simple.pkl --track_running_stats 1 --workers 0 \
+      python3 ./prune_etenas.py --save_dir "${SAVE_FOLDER}/${method[0]}/${output_path}" --max_nodes 4 \
+        --dataset ${dataset} --data_path "${BASE_DATAPATH}/${data_path}" --search_space_name nas-bench-201 --super_type basic \
+        --arch_nas_dataset ${BASE_DATAPATH}/NAS-Bench-201-v1_0-e61699-simple.pkl --track_running_stats 1 --workers 0 \
         --precision 3 --init kaiming_norm --repeat 3 --batch_size 72 --prune_number 1 \
         --rankers_config "configs/rankers_configs/${method[1]}"
     done
   done
 done
+
+deactivate
